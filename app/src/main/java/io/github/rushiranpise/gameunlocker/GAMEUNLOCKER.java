1c22439c51e10cefaf01149118eccc4a90aaa069
Chitransh <chaudharichitransh9@gmail.com>
2025-01-26 19:54:18 +0530

    Back to redmagic test Update GAMEUNLOCKER.java

--- app/src/main/java/io/github/rushiranpise/gameunlocker/GAMEUNLOCKER.java
+++ app/src/main/java/io/github/rushiranpise/gameunlocker/GAMEUNLOCKER.java
@@ -46,8 +46,8 @@ public class GAMEUNLOCKER implements IXposedHookLoadPackage {
     // samsung
     // Props to Spoof as Samsung Galaxy On7
     private static void propsToChangeSamsungGalaxyOn7() {
-        setPropValue("MANUFACTURER", "nubia");
-        setPropValue("MODEL", "NX729J");
+        setPropValue("MANUFACTURER", "samsung");
+        setPropValue("MODEL", "SM-G600S");
     }
 
     private static void setPropValue(String key, Object value) {
